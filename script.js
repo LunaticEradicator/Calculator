3d433cb3259642dc0075ec476d50b2e2c1e65d90
YeduKrishnan A <kuttuzz17@gmail.com>
2022-09-07 13:20:33 +0530

    Create Base Style File

---
+++ script.js
@@ -0,0 +1,48 @@
+const main = document.querySelector(".main");
+const content = document.querySelector(".content");
+
+// function row() {
+//     for (let i = 0; i < 3 * 3; i++) {
+//         let sum = 1;
+//         const div = document.createElement("div");
+//         cal.style.gridTemplateColumns = "repeat(3, 1fr)";
+//         cal.style.gridTemplateRows = "repeat(3, 1fr)";
+//         div.classList.add("num");
+
+//         cal.append(div);
+//     }
+// }
+// row();
+
+
+
+
+
+
+const addNum = function (a, b) {
+    const result = a + b;
+    return result;
+}
+const subtractNum = function (a, b) {
+    const result = a - b;
+    return result;
+}
+const multipleNum = function (a, b) {
+    const result = a * b;
+    return result;
+}
+const divideNum = function (a, b) {
+    const result = a / b;
+    return result;
+}
+
+// number + number = result
+const operator = function (numOne, numTwo) {
+    numOne = btn.textContent;
+    // user select a Number
+    // user select an operator
+    // user select a number again
+    // user press = sign
+    // get result
+
+}
